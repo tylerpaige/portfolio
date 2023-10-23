9750f899e40094e787f7ca184e2f85852d0d5847
Tyler Paige <tylerclarkpaige@gmail.com>
2023-10-22 21:53:52 -0400

    Get build passing

--- app/components/Error.js
+++ app/components/Error.js
@@ -21,7 +21,7 @@ export function Error({ title, children, className, ...props }) {
 export function NotFoundError(props) {
   return (
     <Error title="404" {...props}>
-      <p>There's nothing written here.</p>
+      <p>There’s nothing written here.</p>
       <p>You write instead: self@tylerpaige.com</p>
     </Error>
   );
